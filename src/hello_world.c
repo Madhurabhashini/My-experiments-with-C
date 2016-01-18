adc23a513eff257a9bc093fda6f4c5ef26e2f07a
Madhurabhashini <madhura.24889@gmail.com>
2016-01-18 21:43:11 +0100

    Hello World Program

---
+++ src/hello_world.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+{
+	printf("\n\
+		  ***********************************\n \
+		  Welcome to C Programming with Git!!\n \
+		  ***********************************\n\n");
+}
